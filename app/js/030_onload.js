95ab248181a3e6074e362498eb8a6471b10e1b32
David Nicholson <me@djnicholson.com>
2019-01-08 14:18:25 +0000

    code cleanup

--- app/js/030_onload.js
+++ app/js/030_onload.js
@@ -1,17 +1,6 @@
 blockslack.onload = (function(){
-    
-    // privates:
-    // var foo = ...
-    // var bar = ...    
-
-    // initialization:
-    // (don't depend on other packages, order of package initialization is not guaranteed)
-    // foo = 1;
-    // bar = 2;
 
     return {
-
-        // publics:
         
         go: function() {
             blockslack.authentication.initialize();
