447ff373e0e1c2538baedd12dfe3f75da54f87bb
David Nicholson <me@djnicholson.com>
2019-01-09 19:19:50 +0000

    pubsub working

--- app/js/030_pubsub.js
+++ app/js/030_pubsub.js
@@ -1,18 +1,83 @@
 blockslack.pubsub = (function(){
     
-    // privates:
-    // var foo = ...
-    // var bar = ...
+    var Connection = function(serverUrl) {
+        this.serverUrl = serverUrl;
+        this.connection = undefined;
+        this.subscribed = undefined;
+        
+        (this.ensureConnected = function() {
+            if (!this.connection || this.connection.readyState > 1) {
+                this.connection = new WebSocket(serverUrl);
+                this.subscribed = { };
+                this.connection.onmessage = handleUpdate;
+            }
+        })();
+        
+        this.subscribe = function(feedId) {
+            this.ensureConnected();
+            if (!this.subscribed[feedId]) {
+                try {
+                    this.connection.send(JSON.stringify({ t: "s", f: feedId }));
+                    this.subscribed[feedId] = true;
+                } catch(e) {
+                    console.log("Error subscribing to " + feedId + " on " + this.serverUrl);
+                }
+            }
+        };
 
-    // initialization:
-    // (don't depend on other packages, order of package initialization is not guaranteed)
-    // foo = 1;
-    // bar = 2;
+        this.publish = function(feedId) {
+            this.ensureConnected();
+            try {
+                this.connection.send(JSON.stringify({ t: "p", f: feedId }));
+            } catch(e) {
+                console.log("Error publishing to " + feedId + " on " + this.serverUrl);
+            }
+        };
+    };
+
+    var connections = { }
+
+    var decodeFeedId = function(feedId) {
+        var lookupTable = getLookupTable();
+        return lookupTable[feedId];
+    };
+
+    var encodeFeedId = function(hostUserId, filename, keyId) {
+        var lookupTable = getLookupTable();
+        var key = sha256(hostUserId + "_" + filename + "_" + keyId);
+        lookupTable[key] = { hostUserId: hostUserId, filename: filename, keyId: keyId };
+        return key;
+    };
+
+    var getConnection = function(serverUrl) {
+        return connections[serverUrl] || (connections[serverUrl] = new Connection(serverUrl));
+    };
+
+    var getLookupTable = function() {
+        return blockslack.authentication.state("pubsubLookup") || blockslack.authentication.state("pubsubLookup", { });
+    };
+
+    var handleUpdate = function(feedId) {
+        var feedDetails = feedId && decodeFeedId(feedId.data);
+        if (feedDetails) {
+            var hostUserId = feedDetails.hostUserId;
+            var filename = feedDetails.filename;
+            var keyId = feedDetails.keyId;
+            blockslack.feedpub.read(hostUserId, filename, keyId).then(function(feedRoot) {
+                blockslack.polling.consumeFeed(hostUserId, filename, feedRoot);
+            });
+        }
+    };
+
+    var sha256 = function(input) {
+        return sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(input));
+    };
 
     return {
 
         ensureMonitored: function(hostUserId, filename, keyId, serverUrl) {
-            console.log("***", "ensureMonitored", hostUserId, filename, keyId, serverUrl);
+            var connection = getConnection(serverUrl);
+            connection.subscribe(encodeFeedId(hostUserId, filename, keyId));
         },
 
         getServerUrl: function() {
@@ -22,7 +87,8 @@ blockslack.pubsub = (function(){
 
         notifyPublish: function(filename, keyId, serverUrl) {
             var hostUserId = blockslack.authentication.getUsername();
-            console.log("***", "notifyPublish", hostUserId, filename, keyId, serverUrl);
+            var connection = getConnection(serverUrl);
+            connection.publish(encodeFeedId(hostUserId, filename, keyId));
         },
 
     };
