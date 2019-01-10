f23eed0a34808237d11d48d875c1867c5d1cab48
David Nicholson <me@djnicholson.com>
2019-01-10 17:18:41 +0000

    person.userId

--- app/js/030_people.js
+++ app/js/030_people.js
@@ -13,7 +13,7 @@ blockslack.people = (function(){
         person.pending = true;
 
         this.refresh = function() {
-            person.dataReady = blockstack.lookupProfile(userId).then(function (blockstackProfile) {
+            person.dataReady = blockstack.lookupProfile(person.userId).then(function (blockstackProfile) {
                 person.exists = true;
                 person.blockslackUser = blockstackProfile.apps[APP_URL] ? true : false;
                 blockstackProfile.image && blockstackProfile.image.length && (person.image = blockstackProfile.image[0].contentUrl);
