a78a94c596e218fe4480dd6aab836545946a42e6
David Nicholson <me@djnicholson.com>
2019-01-10 01:58:45 +0000

    secure websockets

--- app/js/030_pubsub.js
+++ app/js/030_pubsub.js
@@ -1,6 +1,6 @@
 blockslack.pubsub = (function(){
     
-    var DEFAULT_SERVER = "ws://server.blockslack.io:80";
+    var DEFAULT_SERVER = "wss://d2wtq6yxegunph.cloudfront.net/";
 
     var WARMUP_DELAY = 5000;
     var MAX_ATTEMPTS = 10;
