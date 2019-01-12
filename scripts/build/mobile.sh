34ecbd142b8774b8f2a0b1b0608e0ced3f714980
David Nicholson <me@djnicholson.com>
2019-01-12 20:35:27 +0000

    sign and align mobile app during build

--- scripts/build/mobile.sh
+++ scripts/build/mobile.sh
@@ -7,10 +7,14 @@ MOBILEROOT=$ROOT/mobile
 BUILDROOT=$ROOT/build
 DEPLOYROOT=$ROOT/deploy
 
+KEYSTORE=~/blockslack.keystore
+
 echo Building mobile apps...
 pushd $MOBILEROOT
 cordova prepare
-cordova build
 cordova build --release
 cp platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $DEPLOYROOT/android.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass blockslack $DEPLOYROOT/android.apk blockslack
+~/Library/Android/sdk/build-tools/28.0.3/zipalign -v 4 $DEPLOYROOT/android.apk $DEPLOYROOT/android-aligned.apk
+mv $DEPLOYROOT/android-aligned.apk $DEPLOYROOT/android.apk
 popd
