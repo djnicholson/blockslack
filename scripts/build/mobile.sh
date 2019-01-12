b7447be5959070f42b23963300d1d0e5e7489cfd
David Nicholson <me@djnicholson.com>
2019-01-12 21:35:25 +0000

    iphone app

--- scripts/build/mobile.sh
+++ scripts/build/mobile.sh
@@ -12,7 +12,9 @@ KEYSTORE=~/blockslack.keystore
 echo Building mobile apps...
 pushd $MOBILEROOT
 cordova prepare
-cordova build --release
+cordova-icon
+cordova-splash
+cordova build --release --buildFlag="-UseModernBuildSystem=0"
 cp platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $DEPLOYROOT/android.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass blockslack $DEPLOYROOT/android.apk blockslack
 ~/Library/Android/sdk/build-tools/28.0.3/zipalign -v 4 $DEPLOYROOT/android.apk $DEPLOYROOT/android-aligned.apk
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,3 +5,7 @@ npm install -g babel-minify
 npm install -g dart-sass
 npm install -g http-server
 npm install -g cordova
+npm install -g cordova-icon
+npm install -g cordova-splash
+
+brew install imagemagick
