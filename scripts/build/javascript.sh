48f4089b7cee98373a6a69277182c6917d71cab2
David Nicholson <me@djnicholson.com>
2019-01-18 23:56:39 +0000

    ipad debugging

--- scripts/build/javascript.sh
+++ scripts/build/javascript.sh
@@ -8,5 +8,7 @@ DEPLOYROOT=$ROOT/deploy
 
 echo -n Crunching JavaScript...
 cat $APPROOT/js/*.js > $BUILDROOT/all.js
+babel --compact false $BUILDROOT/all.js > $BUILDROOT/all.no-es6.js
 minify $BUILDROOT/all.js -o $DEPLOYROOT/all.js 
+minify $BUILDROOT/all.no-es6.js -o $DEPLOYROOT/all.no-es6.js 
 echo Done
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 npm install -g html-minifier
+npm install -g babel-cli
 npm install -g babel-minify
 npm install -g dart-sass
 npm install -g http-server
