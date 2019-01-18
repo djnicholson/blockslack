6905f3a865f359c9c1ad0641c486b5cf9f755493
David Nicholson <me@djnicholson.com>
2019-01-18 22:58:24 +0000

    create mac installer

--- scripts/build/mobile.sh
+++ scripts/build/mobile.sh
@@ -14,9 +14,13 @@ pushd $MOBILEROOT
 cordova prepare
 cordova-icon
 cordova-splash
-cordova build --release --buildFlag="-UseModernBuildSystem=0"
+cordova build android --release 
+cordova build ios --release --buildFlag="-UseModernBuildSystem=0"
+cordova build osx --release 
 cp platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $DEPLOYROOT/android.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass blockslack $DEPLOYROOT/android.apk blockslack
 ~/Library/Android/sdk/build-tools/28.0.3/zipalign -v 4 $DEPLOYROOT/android.apk $DEPLOYROOT/android-aligned.apk
 mv $DEPLOYROOT/android-aligned.apk $DEPLOYROOT/android.apk
+rm $DEPLOYROOT/blockslack.dmg
+hdiutil create -volname blockslack-mac -srcfolder platforms/osx/build/blockslack.app -ov -format UDZO $DEPLOYROOT/blockslack.dmg
 popd
